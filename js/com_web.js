7a68c4da7a33f11f694f22f888015d75218d9964
Dylan.Blue <bindablue@gmail.com>
2026-09-04 17:39:28 +0800

    路径

--- js/com_web.js
+++ js/com_web.js
@@ -45,7 +45,7 @@ if (typeof nologin == "undefined") {//如果没有定义nologin则需要登录
 	//alert(JSON.stringify(reqObj));
 	postData(
 			reqObj,
-			'/service/user/userAccount/renewal',
+			'/user/userAccount/renewal',
 			function(data) {
 				if (data["CODE"] != "0000") {
 					if (isWeiXin()) {
