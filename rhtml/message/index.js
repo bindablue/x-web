b04a54688efe9194b25afa561dac9bd2122e76dc
Dylan.Blue <bindablue@gmail.com>
2026-09-04 18:15:47 +0800

    首页活动

--- rhtml/message/index.js
+++ rhtml/message/index.js
@@ -1,8 +1,8 @@
 //var messages="通知：<span onclick=\"webGotoRemote('https://dododido.cn/service/v6.html')\">1、v6.1.0已发布,NFC快捷打卡,全新体验!&nbsp;&nbsp;</span><span onclick=\"safari('https://dododido.cn/service/agreements.html')\">2、应用协议已变更&nbsp;<i class='glyphicon glyphicon-hand-right'>点击查看</i></span>";
 var messages =`
 <div class="text-center text-primary" style="padding:0px;margin:2px;" 
-        onclick="webGotoRemote('https://dododido.cn/service/food.html')">
+        onclick="webGotoRemote('https://dododido.cn/service/time/actInfo.html?id=3')">
     <span class="glyphicon glyphicon-cutlery"></span>
-    <span class="text-danger glyphicon glyphicon-fire"></span> 【食物热量计算】已上架，欢迎购买体验。
+    <span class="text-danger glyphicon glyphicon-fire"></span>开学季活动，开通年度会员，免费体验7天会员。不满意可以取消续期。
 </div>
 `
